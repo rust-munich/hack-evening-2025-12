7ad9dc18db2344f0f513b2e44dbf108793f87d99
sassman <sassman@users.noreply.github.com>
2025-12-10 14:55:01 +0000

    deploy: 70f31daba380be9e43cdfb365f4c3cb443711b1c

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-fd902783.js',
+                path_to_root + 'searchindex-18a490f5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
